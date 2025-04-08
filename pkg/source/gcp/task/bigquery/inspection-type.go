3ff0cfce15787a8b1c100295ec904a056ad70695
RyuSA <12961775+RyuSA@users.noreply.github.com>
2025-04-08 09:23:11 +0000

    Create a new InspectionType

---
+++ pkg/source/gcp/task/bigquery/inspection-type.go
@@ -0,0 +1,31 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bigquery
+
+import (
+	"math"
+
+	"github.com/GoogleCloudPlatform/khi/pkg/inspection"
+)
+
+var InspectionTypeId = "gcp-bigquery"
+
+var BigQueryInspectionType = inspection.InspectionType{
+	Id:          InspectionTypeId,
+	Name:        "BigQuery",
+	Description: `Visualize BigQuery Job. This inspection allows you to see all BigQuery jobs whitin the specified project.`,
+	Icon:        "assets/icons/composer.webp",
+	Priority:    math.MaxInt - 2,
+}
